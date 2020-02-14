28345a916edb4e35e20dd4fb690f827c456d5593
mutwakilmo <mutwakilmo@gmail.com>
2020-02-14 12:08:24 +0100

    add IcePick: To make it easier to work with bundles

--- app/src/main/java/com/mutwakilmo/android/mynews/WelcomeActivity.java
+++ app/src/main/java/com/mutwakilmo/android/mynews/WelcomeActivity.java
@@ -16,6 +16,7 @@ import butterknife.OnClick;
 public class WelcomeActivity extends AppCompatActivity {
     // Class name for Log tag
     public static final String LOG_TAG = WelcomeActivity.class.getSimpleName();
+
     @BindView(R.id.animation_view1)
     LottieAnimationView animationView1;
     @BindView(R.id.animation_view2)
@@ -33,7 +34,7 @@ public class WelcomeActivity extends AppCompatActivity {
 
     @OnClick(R.id.start_btn)
     public void onViewClicked() {
-        Log.d(LOG_TAG, "onViewClicked: start btn");
+        Log.d(LOG_TAG, "onViewClicked: Welcome start btn");
         Toast.makeText(this,"welcome to MyNews app", Toast.LENGTH_SHORT).show();
         Intent i = new Intent(this,MainActivity.class);
         startActivity(i);
