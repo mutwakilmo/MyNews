47e781eac434dfa5d090294820abd0a061fc8223
mutwakilmo <mutwakilmo@gmail.com>
2020-02-20 11:03:28 +0100

    Run your first network query

--- app/src/main/java/com/mutwakilmo/android/mynews/MainActivity.java
+++ app/src/main/java/com/mutwakilmo/android/mynews/MainActivity.java
@@ -6,9 +6,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private MainFragment mainFragment;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.configureAndShowMainFragment();
+    }
+
+    // -------------------
+    // CONFIGURATION
+    // -------------------
+
+    private void configureAndShowMainFragment(){
+
+        mainFragment = (MainFragment) getSupportFragmentManager().findFragmentById(R.id.activity_main_frame_layout);
+
+        if (mainFragment == null) {
+            mainFragment = new MainFragment();
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.activity_main_frame_layout, mainFragment)
+                    .commit();
+        }
     }
 }
---
+++ app/src/main/java/com/mutwakilmo/android/mynews/MainFragment.java
@@ -0,0 +1,42 @@
+package com.mutwakilmo.android.mynews;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MainFragment extends Fragment {
+
+    // FOR DESIGN
+    @BindView(R.id.fragment_main_textview)
+    TextView textView;
+
+    public MainFragment() { }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    // -----------------
+    // ACTIONS
+    // -----------------
+
+    @OnClick(R.id.fragment_main_button)
+    public void submit(View view) { }
+
+}
