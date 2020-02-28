6d7235f7785c56fd42aa946f86ec7c677906f838
mutwakilmo <mutwakilmo@gmail.com>
2020-02-28 09:38:11 +0100

    Navigation drawer style

--- app/src/main/java/com/mutwakilmo/android/mynews/MainActivity.java
+++ app/src/main/java/com/mutwakilmo/android/mynews/MainActivity.java
@@ -4,22 +4,11 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity  {
-
-
-
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
-
-    // -------------------
-    // CONFIGURATION
-    // -------------------
-
-
-
 }
