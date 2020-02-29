c8f944b770b1938c89f86a6a49bf1838220805d9
mutwakilmo <mutwakilmo@gmail.com>
2020-02-29 15:24:40 +0100

    NYT constants

---
+++ app/src/main/java/com/mutwakilmo/android/mynews/NYTConstants.java
@@ -0,0 +1,16 @@
+package com.mutwakilmo.android.mynews;
+
+/**
+ * Created by Mutwakil Mo on 28/02/2020
+ */
+public final class NYTConstants {
+
+    public static final String[] NEWS_SECTIONS = {
+            "Top Stories", "Most Popular", "Technology",
+            "Business", "Sport", "Travel", "Fashion",
+            "Science", "Automobiles", "Theatre", "Arts",
+            "World", "Health", "Food"
+    };
+
+    public static final String NYT_SECTION_NAME = "NYT_SECTION_NAME";
+}
